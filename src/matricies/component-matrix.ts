aca500714ebac4e6a416e9ede6212c8e22bf5e01
Alex Van Camp <vancamp.alex@gmail.com>
2018-12-18 17:15:00 -0600

    feat(component-matrix): migrate to using an Extron Crosspoint 88

--- src/matricies/component-matrix.ts
+++ src/matricies/component-matrix.ts
@@ -1,9 +1,8 @@
 // Ours
 import {AbstractMatrix} from './abstract-matrix';
 import config from '../config';
-import {SOCKET_MESSAGES} from '../../types/socket';
 
-const ACK_REGEX = /SBUD\d(\d)O(\d)/;
+const OUTPUT_REGEX = /\d{2}/g;
 
 export class ComponentMatrix extends AbstractMatrix {
 	name = 'Component';
@@ -11,46 +10,32 @@ export class ComponentMatrix extends AbstractMatrix {
 	// Serial connection
 	serialPortPath = config.get('componentSerialPath');
 	baudRate = 9600;
-	byteLength = 8;
 
 	// Serial protocol
-	fullUpdateTriggers = null;
-	fullUpdateRequest = null;
-	fullUpdateResponse = null;
-	delimiter = '';
+	fullUpdateTriggers = [
+		/^RECONFIG$/, // When the user changes something via the front panel.
+		/^\(c\)Copyright \d{4}, Extron Electronics/ // When the unit powers up.
+	];
+	fullUpdateRequest = 'V0.';
+	fullUpdateResponse = /^(?:\d{2} ){8}Vid (?:\d{2} ){8}Aud$/;
 
 	constructor() {
 		super();
 		this.init();
-
-		this.serialport.on('open', async () => {
-			// Reset the matrix to a known state.
-			this._addCommandToQueue('SBALLRST');
-
-			// Lock the front panel so only we can control it via serial.
-			this._addCommandToQueue('SBSYSMLK');
-		});
-
-		this.parser.on('data', (data: string) => {
-			const str = data.toString();
-			const matches = str.match(ACK_REGEX);
-			if (matches) {
-				const output = parseInt(matches[2], 10) - 1;
-				const input = parseInt(matches[1], 10) - 1;
-				this.state.outputs[output] = input;
-				this.emit(SOCKET_MESSAGES.OUTPUT_STATUSES, this.state.outputs);
-			} else if (str === 'SBRSTACK') {
-				this.state.outputs = [0, 0, 0, 0, 0, 0, 0, 0];
-				this.emit(SOCKET_MESSAGES.OUTPUT_STATUSES, this.state.outputs);
-			}
-		});
 	}
 
-	processFullUpdate(_data: string) {
+	processFullUpdate(data: string) {
+		const matches = data.match(OUTPUT_REGEX);
+		if (matches) {
+			return matches.slice(0, 8).map(match => {
+				return parseInt(match, 10) - 1;
+			});
+		}
+
 		return this.state.outputs;
 	}
 
 	protected _buildSetOutputCommand(output: number, input: number) {
-		return `SBI0${input + 1}O0${output + 1}`;
+		return `${input + 1}*${output + 1}!`;
 	}
 }
